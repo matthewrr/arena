021d3634bc1a2edf19c89ed197a1b8f8b6fb772d
matthewrr <matthewrr@users.noreply.github.com>
2018-02-20 17:01:52 +0000

    Add basic search for product title and description

--- search/views.py
+++ search/views.py
@@ -9,16 +9,30 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from .filters import UserFilter
 
+from functools import reduce
+import operator
+
 class SearchProductView(ListView):
     template_name = "search/view.html"
     
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        print(request.GET)
         query = request.GET.get('q')
+        result = Product.objects.all() #added
+        
+        print(request.GET)
         print(query)
+        
         if query is not None:
-            return Product.objects.all()
+            query_list = query.split()
+            result = result.filter(
+                reduce(operator.and_,
+                       (Q(title__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                       (Q(description__icontains=q) for q in query_list))
+            )
+        return result
+            #return Product.objects.all()
             
 def search(request):
     user_list = User.objects.all()
