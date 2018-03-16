93dd62fe2ae178aa563c4099152bfd54228a30b0
matthewrr <matthewrr@users.noreply.github.com>
2018-03-16 20:20:23 +0000

    Add form validation to modal's section locator

--- locations/admin.py
+++ locations/admin.py
@@ -4,7 +4,7 @@ from .models import Location
 
 # Register your models here.
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ['stand_name','stand_location', 'active', 'featured', 'outside_vendor']
+    list_display = ['stand_name','stand_location', 'stand', 'active', 'featured', 'outside_vendor']
     search_fields = ['stand_name','stand_location']
     
 admin.site.register(Location, LocationAdmin)
